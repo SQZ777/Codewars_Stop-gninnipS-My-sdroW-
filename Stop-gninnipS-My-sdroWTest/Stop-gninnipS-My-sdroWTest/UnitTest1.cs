4c8c7aeefb682cc7124e686c633e4ee2a2743054
sqz777 <sqz777isme@gmail.com>
2017-07-23 01:27:50 +0800

    first Failed

--- Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
+++ Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
@@ -7,8 +7,20 @@ namespace Stop_gninnipS_My_sdroWTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_Empty_Should_Be_Empty()
         {
+            var input = string.Empty;
+            var expected = string.Empty;
+            var actual = Kata.spinWords(input);
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Kata
+    {
+        public static string spinWords(string input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
