2da516d13509c4c91a6f6376d3fe41a6b30afc45
sqz777 <sqz777isme@gmail.com>
2017-07-23 01:38:38 +0800

    refactor Test Code

--- Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
+++ Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
@@ -10,19 +10,19 @@ namespace Stop_gninnipS_My_sdroWTest
         [TestMethod]
         public void Input_Empty_Should_Be_Empty()
         {
-            var input = string.Empty;
-            var expected = string.Empty;
-            var actual = Kata.spinWords(input);
-            Assert.AreEqual(expected,actual);
+            spinResult(string.Empty,string.Empty);
         }
 
         [TestMethod]
         public void Input_aAAa_Should_Be_aAAa()
         {
-            var input = "aAAa";
-            var expected = "aAAa";
+            spinResult("aAAa", "aa");
+        }
+
+        private static void spinResult(string input, string expected)
+        {
             var actual = Kata.spinWords(input);
-            Assert.AreEqual(expected,actual);
+            Assert.AreEqual(expected, actual);
         }
     }
 
