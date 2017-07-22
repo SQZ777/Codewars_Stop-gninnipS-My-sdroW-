cf50bab2a4aee6dfa7f6d5b9d82f64f8d672f81a
sqz777 <sqz777isme@gmail.com>
2017-07-23 01:20:25 +0800

    initial

---
+++ Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stop_gninnipS_My_sdroWTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
