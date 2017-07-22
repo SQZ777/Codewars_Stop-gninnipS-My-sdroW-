53508c719e0883ef4e8cbfef0ab67cd28ea9cca3
sqz777 <sqz777isme@gmail.com>
2017-07-23 02:16:44 +0800

    pass test case:input Hello return olleH

--- Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
+++ Stop-gninnipS-My-sdroWTest/Stop-gninnipS-My-sdroWTest/UnitTest1.cs
@@ -10,15 +10,21 @@ namespace Stop_gninnipS_My_sdroWTest
         [TestMethod]
         public void Input_Empty_Should_Be_Empty()
         {
-            spinResult(string.Empty,string.Empty);
+            spinResult(string.Empty, string.Empty);
         }
 
         [TestMethod]
         public void Input_aAAa_Should_Be_aAAa()
         {
-            spinResult("aAAa", "aa");
+            spinResult("aAAa", "aAAa");
         }
 
+        [TestMethod]
+        public void Input_Hello_Should_Be_olleH()
+        {
+            spinResult("Hello", "olleH");
+        }
+        
         private static void spinResult(string input, string expected)
         {
             var actual = Kata.spinWords(input);
@@ -30,6 +36,12 @@ namespace Stop_gninnipS_My_sdroWTest
     {
         public static string spinWords(string input)
         {
+            if (input.Length > 4)
+            {
+                char[] inputArray = input.ToCharArray();
+                Array.Reverse(inputArray);
+                return new string(inputArray);
+            }
             return input;
         }
     }
